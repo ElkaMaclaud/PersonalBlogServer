76a2068f9b09caeb64e91010a6a23a4029a0bb25
ElkaMaclaud <elkaMaclaud@gmail.com>
2024-03-19 12:46:30 +0300

    -

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const authRouter = require("./authRouter");
 const PORT = process.env.PORT || 5000;
 
+const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRouter);
