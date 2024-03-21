f0f1706183fb768d6a217fc808e4913f1f66aba4
ElkaMaclaud <elkaMaclaud@gmail.com>
2024-03-21 19:52:36 +0300

    change model Data and re-recorded data in db

--- models/Data.js
+++ models/Data.js
@@ -2,8 +2,8 @@ const { Schema, model } = require("mongoose");
 
 const Data = new Schema({
   resume: { type: Object, ref: "Resume" },
-  posts: [{ type: Array, ref: "Post" }],
-  works: [{ type: Array, ref: "Work" }],
+  posts: [{ type: Object, ref: "Post" }],
+  works: [{ type: Object, ref: "Work" }],
 });
 
 module.exports = model("Data", Data);
